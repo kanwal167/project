28e92748255d7f105eda613ec6d5baf0ef30c218
kanwal167 <kanwalislam025@gmail.com>
2024-12-23 12:17:31 +0500

    Add files via upload

--- pres.py
+++ pres.py
@@ -1,9 +1,9 @@
-#the quiz program is a simple user that asks the user a series of question and keeps track of their score...
+#the quiz program is a simple program that asks the user a series of question and keeps track of their score...
 #list of questions and answers 
 questions =[{"question":"what is the syntax of define the function?","answer":"def name():"},
-            {"question":"what stands for oop in python?","answer":"object oriented program"},
-            {"question":"what you mean by dunder in python?","answer":"double underscore"},
-            {"question":"the process to solve the error what we called in python?","answer":"debugging"}
+            {"question":"what stands for oop in python","answer":"object oriented program"},
+            {"question":"what you mean by dunder in python","answer":"double underscore"},
+            {"question":"the process to solve the error what we called in python","answer":"debugging"}
             ]
 score=0
 for question in questions:
